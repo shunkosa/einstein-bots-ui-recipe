8e15d5a496a177732fb84ac92063b52ce9a4c838
shunkosa <shun.kosaka.102@gmail.com>
2019-07-28 13:52:25 +0900

    Retrieve meta ogp

--- force-app/main/default/lwc/chatMessage/chatMessage.js
+++ force-app/main/default/lwc/chatMessage/chatMessage.js
@@ -1,33 +1,60 @@
 import BaseChatMessage from 'lightningsnapin/baseChatMessage';
 import { track } from 'lwc';
+import getMetaProperties from '@salesforce/apex/OGPParser.getMetaProperties'
 
 const DEFAULT_MESSAGE_PREFIX = 'PLAIN_TEXT';
 const RICHTEXT_MESSAGE_PREFIX = 'RICH_TEXT';
 const YOUTUBE_MESSAGE_PREFIX = 'YOUTUBE';
 const IMAGE_MESSAGE_PREFIX = 'IMAGE';
-const SUPPORTED_MESSAGE_PREFIX = [DEFAULT_MESSAGE_PREFIX, RICHTEXT_MESSAGE_PREFIX, YOUTUBE_MESSAGE_PREFIX, IMAGE_MESSAGE_PREFIX];
+const URL_MESSAGE_PREFIX = 'URL'
+const SUPPORTED_MESSAGE_PREFIX = [DEFAULT_MESSAGE_PREFIX, RICHTEXT_MESSAGE_PREFIX, YOUTUBE_MESSAGE_PREFIX, IMAGE_MESSAGE_PREFIX, URL_MESSAGE_PREFIX];
 
 /**
  * Displays a chat message using the inherited api messageContent and is styled based on the inherited api userType and messageContent api objects passed in from BaseChatMessage.
  */
 export default class ChatMessageDefaultUI extends BaseChatMessage {
     messageType = DEFAULT_MESSAGE_PREFIX;
-    @track singleContent = '';
-   
+    @track content = '';
+    @track ogpMeta = {};
     connectedCallback() {
         if (!this.isAgent) {
             return;
         }
-        
+        console.log(this.messageContent.value);
         const messageTypePrefixPosition = SUPPORTED_MESSAGE_PREFIX.indexOf(this.messageContent.value.split(':')[0]);
         if (messageTypePrefixPosition > -1) {
             this.messageType = SUPPORTED_MESSAGE_PREFIX[messageTypePrefixPosition];
         }
-        if (this.isYoutube) {
-            this.singleContent = 'https://www.youtube.com/embed/' + this.messageContent.value.split(':')[1];
+        const contentValue = (this.messageContent.value.split(this.messageType + ':').length === 1) ? this.messageContent.value : this.messageContent.value.split(this.messageType + ':')[1];
+        if (this.isPlainText) {
+            this.content = contentValue;
+        } else if (this.isYoutube) {
+            this.content = 'https://www.youtube.com/embed/' + contentValue
+        } else if (this.isImage) {
+            this.content = this.extractOriginalString(contentValue);
+        } else if (this.isUrl) {
+            this.content = this.extractOriginalString(contentValue);
+            getMetaProperties({ url : this.content })
+            .then(result => {
+                this.ogpMeta = result;
+            })
+            .catch(error => {
+                console.log(error.body);
+            });
         } else {
-            this.singleContent = this.messageContent.value.replace(this.messageType + ':', '').replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&quot;/g,'\\"');
+            this.content = contentValue
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&quot;/g, '\\"');
         }
+    }
+
+    extractOriginalString(generatedString) {
+        const matched = generatedString.match(/<a href.+>(.*?)<\/a>/);
+        if (matched.length > 1) {
+            return matched[1];
+        }
+        return generatedString;
     }
 
     get isAgent() {
@@ -48,5 +75,9 @@ export default class ChatMessageDefaultUI extends BaseChatMessage {
 
     get isImage() {
         return this.messageType === IMAGE_MESSAGE_PREFIX;
+    }
+
+    get isUrl() {
+        return this.messageType === URL_MESSAGE_PREFIX;
     }
 }
